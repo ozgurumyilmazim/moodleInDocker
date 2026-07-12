7ea504bc9cc374621393c73b77b5795ff0462e23
Ozgur YILMAZ <ozgury>
2026-07-12 19:02:16 +0300

    update router

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -44,6 +44,8 @@ $CFG->reverseproxy = true;
 if (!empty($_SERVER['HTTP_CF_CONNECTING_IP'])) {
     $_SERVER['REMOTE_ADDR'] = $_SERVER['HTTP_CF_CONNECTING_IP'];
 }
+$CFG->routerconfigured = true;
+
 EOF
     # --- ENJEKSİYON SONU ---
 
